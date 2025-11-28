3295d3e91460718cfcbdfb553305fb516e09c12d
JCantu311 <155592365+JCantu311@users.noreply.github.com>
2025-11-27 18:15:09 -0600

    Separated JavaScript and CSS from HTML

---
+++ buttonFunctionality.js
@@ -0,0 +1,10 @@
+function controlsButton() {
+    document.write("<style> body {background-color: black; color: white; font-family: Arial, sans-serif; margin: 20px;}</style><h1>Controls</h1><p>- Type the character found within brackets '[]' to use the commands on screen.</p><div><button id='returnButton' onclick='returnButton()'>Return</button></div>");
+}
+function aboutButton() {
+    
+}
+
+function returnButton() {
+    location.reload();
+}
